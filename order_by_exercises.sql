6ee315721f85bae78708bd540e77246893c5d645
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-09-08 15:27:34 -0500

    Completed MySQL: MySQL Clauses - Order By & Limit Exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -17,47 +17,7 @@ ORDER BY first_name, last_name;
 SELECT *
 FROM employees
 WHERE first_name IN('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, first_name;
-
-# Find all employees whose last name starts with 'E'
-# — 7,330 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%';
-
-# Find all employees hired in the 90s
-# — 135,214 rows.
-SELECT *
-FROM employees
-WHERE hire_date
-          BETWEEN '1990-01-01' AND '1999-12-31';
-
-# Find all employees born on Christmas
-# — 842 rows.
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%-12-25';
-
-# Find all employees with a 'q' in their last name
-# — 1,873 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
-
-# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
-SELECT *
-FROM employees
-WHERE first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya';
-
-# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
-SELECT *
-FROM employees
-WHERE (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-  AND gender = 'M';
+ORDER BY last_name DESC, first_name DESC;
 
 # Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT *
@@ -94,9 +54,3 @@ WHERE hire_date
   AND birth_date LIKE '%-12-25'
 ORDER BY birth_date,
          hire_date DESC;
-
-# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
